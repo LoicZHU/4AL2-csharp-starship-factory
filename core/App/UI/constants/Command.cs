105abe541bf2a0f6a5b43f91235695ee2c8d8baf
LoicZHU <zhu.loic@gmail.com>
2024-05-01 20:21:29 +0200

    tbc

--- core/App/UI/constants/Command.cs
+++ core/App/UI/constants/Command.cs
@@ -2,10 +2,11 @@ namespace core.App.UI.constants;
 
 public static class Command
 {
+	public const string Help = "HELP";
+	public const string Exit = "EXIT";
 	public const string Instructions = "INSTRUCTIONS";
 	public const string Stocks = "STOCKS";
 	public const string UserInstruction = "[USER_INSTRUCTION]";
 	public const string UserInstructions = "USER_INSTRUCTIONS";
-	public const string Help = "HELP";
-	public const string Exit = "EXIT";
+	public const string Verify = "VERIFY";
 }
---
+++ core/App/UI/constants/Verification.cs
@@ -0,0 +1,8 @@
+namespace core.App.UI.constants;
+
+public static class Verification
+{
+	public const string Error = "ERROR";
+	public const string Available = "AVAILABLE";
+	public const string Unavailable = "UNAVAILABLE";
+}
