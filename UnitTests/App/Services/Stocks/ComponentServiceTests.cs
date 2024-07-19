ab34e30f8c58cec11978cfa78246cf8fe836dc7b
LoicZHU <zhu.loic@gmail.com>
2024-07-19 18:29:10 +0200

    tests: add
    Co-authored-by: zakaryalahmadi <80518856+Zakaryalahmadi@users.noreply.github.com>
    Co-authored-by: Mazene-ZERGUINE <92375034+Mazene-ZERGUINE@users.noreply.github.com>

---
+++ UnitTests/App/Services/Stocks/ComponentServiceTests.cs
@@ -0,0 +1,45 @@
+using core.Components;
+using core.Repositories.ComponentRepository;
+using core.Services;
+using core.Starships;
+using NSubstitute;
+
+namespace UnitTests.App.Services.Stocks;
+
+public class ComponentServiceTests
+{
+	private readonly IComponentRepository _componentRepository;
+	private readonly ComponentService _componentService;
+
+	public ComponentServiceTests()
+	{
+		_componentRepository = Substitute.For<IComponentRepository>();
+		_componentService = new ComponentService(_componentRepository);
+	}
+
+	[Theory]
+	[InlineData(StarshipName.Cargo, HullComponent.HullHc1, 1)]
+	[InlineData(StarshipName.Cargo, EngineComponent.EngineEc1, 1)]
+	[InlineData(StarshipName.Cargo, WingComponent.WingWc1, 2)]
+	[InlineData(StarshipName.Cargo, ThrusterComponent.ThrusterTc1, 1)]
+	[InlineData(StarshipName.Explorer, HullComponent.HullHe1, 1)]
+	[InlineData(StarshipName.Explorer, EngineComponent.EngineEe1, 1)]
+	[InlineData(StarshipName.Explorer, WingComponent.WingWe1, 2)]
+	[InlineData(StarshipName.Explorer, ThrusterComponent.ThrusterTe1, 1)]
+	[InlineData(StarshipName.Speeder, HullComponent.HullHs1, 1)]
+	[InlineData(StarshipName.Speeder, EngineComponent.EngineEs1, 1)]
+	[InlineData(StarshipName.Speeder, WingComponent.WingWs1, 2)]
+	[InlineData(StarshipName.Speeder, ThrusterComponent.ThrusterTs1, 2)]
+	public void GetComponentsOutFromStock_RemovesCorrectComponents(
+		String starshipName,
+		String componentName,
+		Int32 quantity
+	)
+	{
+		// Act
+		_componentService.GetComponentsOutFromStock(starshipName);
+
+		// Assert
+		_componentRepository.Received(quantity).Remove(componentName);
+	}
+}
---
+++ UnitTests/App/Services/Stocks/StarshipServiceTests.cs
@@ -0,0 +1,125 @@
+using core.Components;
+using core.Repositories.ComponentRepository;
+using core.Services;
+using core.Starships;
+using NSubstitute;
+
+namespace UnitTests.App.Services.Stocks;
+
+public class StarshipServiceTests
+{
+	private readonly IComponentRepository _componentRepository;
+	private readonly StarshipService _starshipService;
+
+	public StarshipServiceTests()
+	{
+		_componentRepository = Substitute.For<IComponentRepository>();
+		_starshipService = new StarshipService(_componentRepository);
+	}
+
+	[Fact]
+	public void GetStarshipSumsFromInput_ValidInput_ReturnsCorrectSums()
+	{
+		// Arrange
+		var input = "1 Explorer, 2 Cargo, 3 Explorer";
+		Action<String> printInvalidCommand = Substitute.For<Action<String>>();
+
+		// Act
+		var result = _starshipService.GetStarshipSumsFromInput(input, printInvalidCommand);
+
+		// Assert
+		Assert.Equal(2, result[StarshipName.Cargo]);
+		Assert.Equal(4, result[StarshipName.Explorer]);
+	}
+
+	[Fact]
+	public void GetStarshipSumsFromInput_InvalidInput_PrintsErrorMessageAndReturnsEmpty()
+	{
+		// Arrange
+		var input = "1 InvalidStarship";
+		var invalidCommandMessage = String.Empty;
+		Action<String> printInvalidCommand = msg => invalidCommandMessage = msg;
+
+		// Act
+		var result = _starshipService.GetStarshipSumsFromInput(input, printInvalidCommand);
+
+		// Assert
+		Assert.Equal("Vaisseau inconnu : Invalidstarship", invalidCommandMessage);
+		Assert.Empty(result);
+	}
+
+	[Theory]
+	[InlineData(
+		StarshipName.Cargo,
+		EngineComponent.EngineEc1,
+		HullComponent.HullHc1,
+		WingComponent.WingWc1,
+		ThrusterComponent.ThrusterTc1
+	)]
+	[InlineData(
+		StarshipName.Explorer,
+		EngineComponent.EngineEe1,
+		HullComponent.HullHe1,
+		WingComponent.WingWe1,
+		ThrusterComponent.ThrusterTe1
+	)]
+	[InlineData(
+		StarshipName.Speeder,
+		EngineComponent.EngineEs1,
+		HullComponent.HullHs1,
+		WingComponent.WingWs1,
+		ThrusterComponent.ThrusterTs1
+	)]
+	public void GetStarshipComponentsCountFromInventories_ValidStarshipName_ReturnsCorrectCounts(
+		String starshipName,
+		String engine,
+		String hull,
+		String wing,
+		String thruster
+	)
+	{
+		// Arrange
+		_componentRepository.GetCountByName(engine).Returns(5);
+		_componentRepository.GetCountByName(hull).Returns(3);
+		_componentRepository.GetCountByName(wing).Returns(8);
+		_componentRepository.GetCountByName(thruster).Returns(2);
+
+		// Act
+		var result = _starshipService.GetStarshipComponentsCountFromInventories(starshipName);
+
+		// Assert
+		Assert.Equal(5, result.Item1);
+		Assert.Equal(3, result.Item2);
+		Assert.Equal(8, result.Item3);
+		Assert.Equal(2, result.Item4);
+	}
+
+	[Fact]
+	public void GetStarshipComponentsCountFromInventories_UnknownStarshipName_ReturnsZeros()
+	{
+		// Arrange
+		var starshipName = "UnknownStarship";
+
+		// Act
+		var result = _starshipService.GetStarshipComponentsCountFromInventories(starshipName);
+
+		// Assert
+		Assert.Equal((0, 0, 0, 0), result);
+	}
+
+	[Fact]
+	public void GetStarshipComponentsCountFromInventories_ExceptionThrown_ReturnsZeros()
+	{
+		// Arrange
+		var starshipName = StarshipName.Cargo;
+		_componentRepository
+			.When(repo => repo.GetCountByName(Arg.Any<string>()))
+			.Throw(new Exception());
+
+		// Act
+		var result = _starshipService.GetStarshipComponentsCountFromInventories(starshipName);
+
+		// Assert
+		Assert.Equal((0, 0, 0, 0), result);
+	}
+}
