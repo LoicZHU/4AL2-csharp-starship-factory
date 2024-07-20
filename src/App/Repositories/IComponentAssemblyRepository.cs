c0a21a1ff26368ef4eb8292ad6c8cf753768b5d8
LoicZHU <zhu.loic@gmail.com>
2024-07-20 11:25:36 +0200

    remove componentAssembly related
    Co-authored-by: zakaryalahmadi <80518856+Zakaryalahmadi@users.noreply.github.com>
    Co-authored-by: Mazene-ZERGUINE <92375034+Mazene-ZERGUINE@users.noreply.github.com>

--- src/App/Repositories/IComponentAssemblyRepository.cs
+++
@@ -1,9 +0,0 @@
-using core.Assemblies;
-
-namespace core.Repositories.ComponentAssemblyRepository;
-
-public interface IComponentAssemblyRepository
-{
-	public void Add(ComponentAssembly componentAssembly);
-	public void AddComponent(Guid id, String component);
-}
